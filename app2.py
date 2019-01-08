b0aecd866c81e5855513f70038dd123b50d6431a
Dasher <asher1234561@gmail.com>
2019-01-08 16:33:06 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,3 +1,4 @@
+#Imports
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy 
 from datetime import datetime
@@ -7,15 +8,15 @@ from wtforms import StringField, PasswordField
 import os
 import BeautifulSoup
 from googlevoice import Voice
-#from googlevoice.util import input
 import base64
+
+
 key = 'abcdefghijklmnopqrstuvwxyz'
 key2 = '123456789'
 # Encryption Fucntion:
 def encrypt(n, plaintext):
 	"""Encrypt the string and return the ciphertext"""
 	result = ''
-
 	for l in plaintext.lower():
 		try:
 			i = (key.index(l) + n) % 26
@@ -38,28 +39,18 @@ def decrypt(n, ciphertext):
 
 	return result
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'jackgod'
-
+# Change the SECRET_KEY to cometing else
+app.config['SECRET_KEY'] = 'Test'
+#"""
 #class Encryption(Form):
 #	encode = StringField('encode', validators=[InputRequired()])
 #	decode = StringField('decode', validators=[InputRequired()]
-
+#""""
 from googlevoice import Voice
 #from googlevoice.util import input
 Email="Your Gmail "
 passwd="Your Gmail Password"
 
-	
-	
-#@app.route('/SMS', methods=['GET','POST'])
-#def sms():
-#	if request.method == 'POST':
-#		text = request.form['text']
-#		number = request.form['number']
-#		print number
-#		return render_template ('sms_show.html', number=number,text=text)
-#	return render_template('sms.html')
-	
 	
 # Route http request to /sms page then POST Data from sms form	
 @app.route('/sms', methods=['GET','POST'])
@@ -80,10 +71,9 @@ def SMS():
 		voice.send_sms(phoneNumber, text)
 	return render_template('sms.html')
 	
-# Home route Accepts POST AND GET Data:	
+# Home route Accepts POST AND GET Methods:	
 @app.route('/', methods=['GET','POST'])
 def index():
-	#form = Encryption()
 	offset = 5
 	if request.method == 'POST':
 		text = request.form['encode']
@@ -94,5 +84,6 @@ def index():
 		return render_template ('encode.html', encode3=encode3,decode3=decode3) # response with decoded and Decrypted data
 		#return render_template ('decode.html', decode3=decode3)
 	return render_template('index.html')	
+#Start Server
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000, debug=False)
